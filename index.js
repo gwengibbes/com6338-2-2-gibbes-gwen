34f5a8f36ff40f59a4e705d9e8964b578d7a9d40
Gwen Gibbes <ggibbes@ufl.edu>
2023-09-03 22:51:27 +0100

    Final

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+// Greet function 
+function greet() {
+// Open window with the text "What is your Name?"
+var name;
+name = prompt("What is your Name?");
+//Alert saying Hello and the person's name. 
+if (name) {
+alert ("Hello, "+name+"")
+} else {
+alert ("Hello")   
+}
+//Prompt asking How old the user is. 
+var ageStr;
+ageStr = prompt("How old are you?");
+//Convert user's answer into a number. 
+var age = parseInt(ageStr, 10);
+//Confirm asking if the user has had a birthday yet this year. 
+birthday = confirm("Have you had a birthday yet this year?");
+var date;
+date = new Date().getFullYear();
+//Alert telling the user what year they" were born in. 
+var birthday;
+if (birthday) {
+alert ("You were born in "+(date-age)+"")}
+else {
+alert ("You were born in "+(date-(age + 1))+"");
+}
+}
+birthdayYet();
